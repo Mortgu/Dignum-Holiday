043870134224603592324b1a6bad72c46fca2990
Oskar Sammet <oskar.sammet@gmail.com>
2025-08-18 11:53:24 +0200

    clean up

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
-import { logout } from "./actions/auth";
 import LoginPage from "./login/page";
-import SignupForm from "./ui/signup-form";
-import { verifySession } from '@/app/lib/dal'
 
 export default async function Home() {
   return (
