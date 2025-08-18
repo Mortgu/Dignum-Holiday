bc33b7a36291b0d7c9727cc869423b3f451b63b0
Oskar Sammet <oskar.sammet@gmail.com>
2025-08-18 11:45:45 +0200

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,10 @@
 import { logout } from "./actions/auth";
+import LoginPage from "./login/page";
 import SignupForm from "./ui/signup-form";
 import { verifySession } from '@/app/lib/dal'
 
 export default async function Home() {
-  const session = verifySession();
-  const user = session?.user;
-
-  console.log(user);
-
-  if (user) {
-    return (
-      <div>
-        Dashboard
-        <button onClick={logout}>Logout</button>
-      </div>
-    )
-  }
-
   return (
-    <SignupForm />
+    <LoginPage />
   )
 }
